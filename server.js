6cfed00a3e79cc94177f86e2a2b0747ce292edf1
AMA <alexanderabram6440@gmail.com>
2020-06-22 18:57:59 -0500

    wrote app.get in server.js

--- server.js
+++ server.js
@@ -10,4 +10,7 @@ app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
-app.get("/notes")
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(mainDir, "notes.html"));
+});
+
