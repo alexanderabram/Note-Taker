2c7095755c491d0c3c6c67f305893f5c1e7c956d
AMA <alexanderabram6440@gmail.com>
2020-06-22 18:51:56 -0500

    added app.use in server.js

--- server.js
+++ server.js
@@ -5,3 +5,7 @@ const fs = require("fs");
 const app = express();
 const port = 8080;
 const mainDir = path.join(__dirname, "/public");
+
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
